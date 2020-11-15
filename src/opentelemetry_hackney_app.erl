5ec2a3bbaed4097496a1c1d59f76be27ea0f40d8
Tristan Sloughter <t@crashfast.com>
2020-11-15 07:17:31 -0700

    register tracer on start of application

--- src/opentelemetry_hackney_app.erl
+++ src/opentelemetry_hackney_app.erl
@@ -10,6 +10,11 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    %% register as a tracer
+    %% this is the only reason we are a runnable application
+    {ok, Vsn} = application:get_key(opentelemetry_hackney, vsn),
+    _ = opentelemetry:register_tracer(opentelemetry_hackney, Vsn),
+
     opentelemetry_hackney_sup:start_link().
 
 stop(_State) ->
